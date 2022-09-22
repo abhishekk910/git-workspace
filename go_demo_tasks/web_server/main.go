cc0f123f6d5902f7f056d0a7917d302c652a992c
Abhishek Krishna Murthy <amurthy@AD.MSYSTECHNOLOGIES.COM>
2022-09-22 11:50:40 +0530

    added files

--- go_demo_tasks/web_server/main.go
+++ go_demo_tasks/web_server/main.go
@@ -7,12 +7,11 @@ import (
 )
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
-	err := r.ParseForm()
-	if err != nil {
-		fmt.Fprintf(w, "ParseForm() err: %v", err)
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v\n", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
+	fmt.Fprintf(w, "POST request successful\n")
 	name := r.FormValue("name")
 	address := r.FormValue("address")
 	fmt.Fprintf(w, "Name = %s\n", name)
