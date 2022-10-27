ff33d5c982c4e6a481f988f0d43431b21bfb3d88
abhishekk910 <abhikrishna.mbl@gmail.com>
2022-10-27 17:19:54 +0530

    added files

--- Go_workspace/linked_list/dll1/main.go
+++ Go_workspace/linked_list/dll1/main.go
@@ -83,18 +83,134 @@ func (dll doublylinkedlist) reversedisplay() {
 	fmt.Printf("Nil\n")
 }
 
+func (dll *doublylinkedlist) sort(){
+	if (dll.head == nil){
+		fmt.Println("Linked List is Empty.")
+	}else{
+		curr := dll.head 
+		for (curr.next != nil){
+			index := curr.next 
+			for (index != nil){
+				if (curr.data > index.data){
+					index.data, curr.data = curr.data, index.data
+				}
+				index = index.next
+			}
+			curr = curr.next
+		}
+	}
+}
+
+func (dll *doublylinkedlist) push_at(data int, index int){
+	new_node := &node{}
+	new_node.data = data 
+	new_node.prev = nil
+	new_node.next = nil 
+	if index < 1{
+		fmt.Println("Index value should be greater than 1")
+	}else if (index == 1){
+		new_node.next = dll.head
+		dll.head = new_node 
+	}else{
+		temp := dll.head 
+		for i:=1; i < index-1; i++{
+			if (temp != nil){
+				temp = temp.next 
+			}
+		}
+		if (temp != nil){
+			new_node.next = temp.next 
+            new_node.prev = temp 
+            temp.next = new_node
+			if (new_node.next != nil){
+				new_node.next.prev = new_node
+			}else{
+				fmt.Println("The previous node is null.")
+			}
+		}
+	}
+}
+
+func (dll *doublylinkedlist) pop_at(index int){
+	if (index < 1){
+		fmt.Println("Index value should be greater than 1")
+	}else if (index == 1){
+		dll.head = dll.head.next 
+	}else{
+		temp := dll.head 
+		for i:=1; i < index-1; i++{
+			if (temp != nil){
+				temp = temp.next 
+			}
+		}
+		if (temp != nil && temp.next != nil){
+			//node_to_delete := temp.next 
+			temp.next = temp.next.next 
+			if (temp.next.next != nil){
+				temp.next.next.prev = temp.next 
+			}
+			//node_to_delete = nil 
+		}else{
+			fmt.Println("The node is null.") 
+		}
+	}
+}
+
+func (dll doublylinkedlist) count_nodes(){
+	temp := dll.head 
+	count := 0
+	for (temp != nil){
+		count += 1
+		temp = temp.next 
+	}
+	fmt.Printf("Number of nodes : %d", count) 
+}
+
+func (dll *doublylinkedlist) delete_all_nodes(){
+	for (dll.head != nil){
+		dll.head = dll.head.next
+	}
+	fmt.Println("All nodes deleted successfully..")
+}
+
+func (dll doublylinkedlist) search_element(search_value int){
+	temp := dll.head 
+	flag := false
+	i := 0
+	if (temp != nil){
+		for (temp != nil){
+			i += 1
+			if (temp.data == search_value){
+				flag = true 
+				break 
+			}
+			temp = temp.next 
+		}
+		if (flag == true){
+			fmt.Printf("%d is found at index %d", search_value, i)
+		}else{
+			fmt.Printf("%d is not found in the list.", search_value) 
+		}
+	}else{
+		fmt.Println("The list is empty.")
+	}
+}
+
 func main(){
 	mylist := doublylinkedlist{}
 	mylist.append(10)
+	mylist.prepend(30) 
 	mylist.append(20)
-	mylist.append(30)
-	mylist.prepend(40)
-	mylist.append(50) 
+	// mylist.display()
+	mylist.sort()
+	// mylist.display()
+	//mylist.reversedisplay()
+	mylist.push_at(100, 2) 
 	mylist.display()
-	// mylist.reversedisplay()
-	mylist.deleteAtFirst()
+	//mylist.pop_at(1) 
 	mylist.display()
-	mylist.deleteAtLast()
-	mylist.display()
+	//mylist.count_nodes()
+	// mylist.delete_all_nodes()
+	// mylist.display()
+	mylist.search_element(100)
 }
-
--- python_workspace/linked_list/csll.py
+++ python_workspace/linked_list/csll.py
@@ -1,3 +1,8 @@
+from email import header
+from os import curdir
+from turtle import position
+
+
 class Node:
     def __init__(self, data):
         self.data = data
@@ -12,17 +17,17 @@ class Circular_Linked_List:
         temp = self.head
         if temp != None:
             while True:
-                print(f"{temp.data} ", end="")
+                print(f"{temp.data}", end="->")
                 temp = temp.next 
                 if (temp == self.head):
                     break 
-            print()
+            print("nil")
         else:
             print("Linked List contains no elements..") 
 
     # adding element at the starting of the linked list.
     def prepend(self, element):
-        new_node = Node(element)
+        new_node = Node(element) 
         if self.head == None:
             self.head = new_node 
             new_node.next = self.head 
@@ -32,7 +37,7 @@ class Circular_Linked_List:
             while (temp.next != self.head):
                 temp = temp.next 
             temp.next = new_node
-            new_node.next = self.head  #current 1st node to 2nd node
+            new_node.next = self.head  #shifting current 1st node to 2nd node
             self.head = new_node   #pointing head to new node .
 
     # adding elments at the end of the linked list.
@@ -105,36 +110,134 @@ class Circular_Linked_List:
                     current = temp #move current to next using temp, repeating the process till current reaches the head.
                 self.head = None #delete the head. 
 
-    
+# This function sorts the list in ascending order
+    def sortList(self):
+        if self.head == None:
+            print("Linked List is Empty.")
+        # Current will point to head 
+        else:
+            curr = self.head 
+            while True:
+                index = curr.next 
+                while (index != self.head):
+                    # If current node is greater than index data, swaps the data    
+                    if (curr.data > index.data):
+                        curr.data, index.data = index.data, curr.data 
+                    index = index.next 
+                curr = curr.next 
+                if (curr.next == self.head):
+                    break 
+
+    def reverseList(self):
+  #1. If head is not null create three nodes
+  #   prevNode - pointing to head,
+  #   tempNode - pointing to head,
+  #   curNode - pointing to next of head
+        if(self.head != None):
+            prevNode = self.head
+            tempNode = self.head
+            curNode = self.head.next
+            
+            #2. assign next of prevNode as itself to make the
+            #   first node as last node of the reversed list
+            prevNode.next = prevNode
+            
+            while(curNode != self.head):
+            #3. While the curNode is not head adjust links 
+            #   (unlink curNode and link it to the reversed list 
+            #   from front and modify curNode and prevNode) 
+                tempNode = curNode.next
+                curNode.next = prevNode
+                self.head.next = curNode
+                prevNode = curNode
+                curNode = tempNode
+
+            #4. Make prevNode (last node) as head
+            self.head = prevNode 
+
+    def push_at(self, element, index):
+        """
+        allocate node to new element and create a temp node to traverse the list
+        """
+        new_node = Node(element)
+        temp = self.head 
+        no_of_elements = 0 
+
+        # find number of elements.
+        if (temp != None):
+            no_of_elements += 1 
+            temp = temp.next 
+        while (temp != self.head):
+            no_of_elements += 1
+            temp = temp.next 
+        
+        # check if adding index is valid.
+        if (index < 1 or index > (no_of_elements+1)):
+            print("Invalid Index.")
+        elif index == 1:
+            # if index is 1, make next of new node as head and new node as head 
+            if (self.head == None):
+                self.head = new_node 
+                self.head.next = self.head 
+            else:
+                while (temp.next != self.head):
+                    temp = temp.next 
+                new_node.next = self.head 
+                self.head = new_node 
+                temp.next = self.head 
+        else:
+            temp = self.head 
+            for i in range(1, index-1):
+                temp = temp.next
+            new_node.next = temp.next 
+            temp.next = new_node 
+
+    # delete a node at the given index.
+    def pop_at(self, index):
+        # create two nodes temp and nodeToDelete to traverse and track the node to delete 
+        nodeToDelete = self.head 
+        temp = self.head 
+        no_of_elements = 0 
+
+        # find the number of elements in the list 
+        if (temp != None):
+            no_of_elements += 1 
+            temp = temp.next 
+        while (temp != self.head):
+            no_of_elements += 1
+            temp = temp.next 
+
+        # check if the specified position is valid 
+        if ((index < 1) or (index > no_of_elements)):
+            print("Invalid Position")
+        elif (index == 1):
+            #  if the index is 1 and head is the only element in the list,  make it null,else make next of head as new head and adjust links accordingly.
+            if (self.head.next == self.head):
+                self.head = None 
+            else:
+                while (temp.next != self.head):
+                    temp = temp.next 
+                self.head = self.head.next 
+                temp.next = self.head 
+                nodeToDelete = None 
+        else:
+            # Else, traverse to node previous to the given index and delete the given node and adjust links accordingly.
+            temp = self.head 
+            for i in range(1, index-1):
+                temp = temp.next
+            nodeToDelete = temp.next 
+            temp.next = temp.next.next 
+            nodeToDelete = None 
+
 
 list1 = Circular_Linked_List()
-# list1.print_list()
-# first = Node(10)
-# list1.head = first 
-# first.next = first 
 
-# second = Node(20)
-# first.next = second
-# second.next = list1.head
-
-# third = Node(30)
-# second.next = third 
-# third.next = list1.head
-# list1.print_list()
-
-
-list1.prepend(20)
-list1.append(30) 
-list1.append(40)
-list1.prepend(10)
-list1.print_list() 
-list1.count_nodes()
-list1.delete_at_start()
+list1.append(10)
+list1.append(20)
+list1.prepend(30)
+list1.prepend(100)
 list1.print_list()
-list1.count_nodes()  
-list1.delete_at_end()
-list1.print_list() 
-list1.count_nodes() 
-list1.delete_all_nodes()
-list1.print_list() 
-list1.count_nodes()
+list1.push_at(200, 2) 
+list1.print_list()
+list1.pop_at(5)
+list1.print_list()
