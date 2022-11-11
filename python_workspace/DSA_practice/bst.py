e9f316b99a290945f73b2db12264b8438ba70a84
abhishekk910 <abhikrishna.mbl@gmail.com>
2022-11-11 18:55:10 +0530

    added files

--- python_workspace/DSA_practice/bst.py
+++ python_workspace/DSA_practice/bst.py
@@ -5,6 +5,8 @@ class BST:
         self.right = None 
 
     def insert(self, data):
+        if self.key == data:
+            return 
         if self.key is None:
             self.key = data 
             return 
@@ -19,15 +21,54 @@ class BST:
             else:
                 self.right = BST(data)
 
+    def search(self, data):
+        if data == self.key:
+            print("Key is present in the tree")
+            return 
+        if data < self.key:
+            if self.left:
+                self.left.search(data)
+            else:
+                print("Key is not present in tree")
+                return
+        else:
+            if self.right:
+                self.right.search(data)
+            else:
+                print("Key is not present in tree")
+                return
+
+    def preorder(self):
+        print(self.key, end=" ")
+        if self.left:
+            self.left.preorder()
+        if self.right:
+            self.right.preorder()
+        
+
+    def inorder(self):
+        if self.left:
+            self.left.inorder()
+        print(self.key, end=" ")
+        if self.right:
+            self.right.inorder()
+
+    def postorder(self):
+        if self.left:
+            self.left.postorder()
+        if self.right:
+            self.right.postorder()
+        print(self.key, end=" ")
+
 root = BST(10)
-list1 = [20, 4, 30, 4, 1, 5, 6]
+# list1 = [100, 20, 5, 10, 150, 25, 120, 125, 200]
+list1 = [6, 3, 1, 6, 7, 3, 98, 97, 101]
+# list1 = [150, 70, 5, 25, 60, 100]
 for i in list1:
     root.insert(i)
-
-# root = BST(10)
-# print(root.key, root.left, root.right)
-# root.left = BST(5)
-# root.right = BST(15)
-# print(root.key, root.left, root.right)
-# print(root.left.key, root.left.left, root.left.right)
-# print(root.right.key, root.right.left, root.right.right)
+root.preorder()
+print()
+root.inorder()
+print()
+root.postorder()
+print()
